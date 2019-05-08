c429d2ec906f44302e247d6c47ace3c10ef9b8bb
alugocp <alugocp@aim.com>
2019-05-08 15:11:00 -0400

    Added some preliminary content

--- www/contact.php
+++ www/contact.php
@@ -1,4 +1,21 @@
 <div class="contact">
   <h1 style="color:var(--blue)">Contact</h1>
+  <p>
+    Feel free to contact us if you have any questions.
+  </p>
+  <div class="icons-wrapper">
+    <div class="icons-col">
+      <img src="res/icons/mail.svg"><br>
+      <a href="mailto:alugocp@gmail.com">alugocp@gmail.com</a><br>
+      <img src="res/icons/mail.svg"><br>
+      <a href="mailto:alugocp@gmail.com">alugocp@gmail.com</a>
+    </div>
+    <div class="icons-col">
+      <img src="res/icons/mail.svg"><br>
+      <a href="mailto:alugocp@gmail.com">alugocp@gmail.com</a>
+    </div>
+    <div class="icons-col">
 
+    </div>
+  </div>
 </div>
